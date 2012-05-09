cf7e9f335be93caa1c45df2913557201022a32e4
Janne Jaakkola <jaakkola.janne@gmail.com>
2012-05-09 11:08:18 +0300

    Possible to edit old textures and models

--- PispalaModelProcessor.cs
+++ PispalaModelProcessor.cs
@@ -1,11 +1,20 @@
 using UnityEditor;
-using System.Collections;
 
 public class PispalaModelProcessor : AssetPostprocessor
 {
 	public void OnPreprocessModel ()
 	{
 		ModelImporter modelImporter = assetImporter as ModelImporter;
+		
+		string[] labels = AssetDatabase.GetLabels(modelImporter);
+		for(int i = 0; i < labels.Length; i++)
+		{
+			if(labels[i] == "imported")
+				return;
+		}
+		
+		AssetDatabase.SetLabels(modelImporter, new string[] {"imported"});
+		
 		modelImporter.importMaterials = false;
 		modelImporter.globalScale = 1;
 	}
--- PispalaTextureProcessor.cs
+++ PispalaTextureProcessor.cs
@@ -1,13 +1,22 @@
 using UnityEditor;
-using System.Collections;
 
 public class PispalaTextureProcessor : AssetPostprocessor
 {
 	public void OnPreprocessTexture ()
 	{
 		TextureImporter textureImporter = assetImporter as TextureImporter;
+		
+		string[] labels = AssetDatabase.GetLabels(textureImporter);
+		for(int i = 0; i < labels.Length; i++)
+		{
+			if(labels[i] == "imported")
+				return;
+		}
+		
+		AssetDatabase.SetLabels(textureImporter, new string[] {"imported"});
+		
 		textureImporter.mipmapEnabled = false;
 		textureImporter.textureFormat = TextureImporterFormat.AutomaticTruecolor;
-		textureImporter.maxTextureSize = 1024;
+		textureImporter.maxTextureSize = 1024;		
 	}
 }
